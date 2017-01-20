179917d7130cd7d6f4d05d5abb3f1372102ce6d4
zjc-enigma <enigma@pku.edu.cn>
2017-01-20 18:23:18 +0000

    12:00, make own super server

--- toutiao/bin/run.py
+++ toutiao/bin/run.py
@@ -2,7 +2,7 @@
 import sys
 reload(sys)
 sys.setdefaultencoding('UTF8')
-sys.path.append('/Users/Patrick/Git')
+sys.path.append('/home/liberty/Git')
 from flask import Flask
 from flask import send_from_directory, request, Response
 from flask import render_template
@@ -18,7 +18,7 @@ import json
 app = Flask(__name__, static_folder="../static", template_folder="../templates")
 
 
-@app.route('/generate')
+@app.route('/demos/ad_crawl/generate')
 def search():
 
     keyword = request.args.get('inputword')
@@ -35,7 +35,7 @@ def search():
 def send_file(filename):
     return send_from_directory(app.static_folder, filename)
 
-@app.route('/')
+@app.route('/demos/ad_crawl')
 def index():
     return render_template('index.tmpl',
                            res_dict=random_select_titles(10),
@@ -43,5 +43,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(threaded=True)
-#    app.run(host="0.0.0.0", threaded=True)
+    #app.run(threaded=True)
+    app.run(host="0.0.0.0", threaded=True)
